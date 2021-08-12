ad204758dbebaa8e81c6ce80180c2bbee3f7941e
vaisakhDev <44577910+vaisakhDev@users.noreply.github.com>
2021-08-12 20:07:10 +0530

    Added new landing page

--- src/ImportFileSteps.js
+++ src/ImportFileSteps.js
@@ -1,3 +1,90 @@
-const ImportFileSteps = () => <div>Import Whatsapp log file....</div>;
+import heroImage from './assets/images/hero.png';
+import guide1 from './assets/images/Guide1.jpg';
+import guide2 from './assets/images/Guide2.jpg';
+import guide3 from './assets/images/Guide3.jpg';
 
+const ImportFileSteps = ({ importChat }) => (
+  <div className="font-content bg-dark text-white px-32 pt-12">
+    <img src={heroImage} alt="Hero" className="absolute top-48 right-72" />
+    <main className="relative">
+      <h1
+        className="font-black leading-7 
+  mb-12 text-title"
+      >
+        <span className="text-primaryDark">Whats</span>Viz
+      </h1>
+      <p className="text-content leading-7 w-3/4 h-1/5 mb-20">
+        WhatsViz is a free and open-source website where you can get fun
+        insights and statistics from your Whatsapp chat data, accompanied by
+        awesome visualizations.
+      </p>
+      <label
+        className="font-extrabold text-black text-content
+         bg-white px-8 py-2 rounded-md border-4 border-primary cursor-pointer"
+      >
+        <span>IMPORT CHAT</span>
+        <input type="file" onChange={importChat} className="hidden" />
+      </label>
+      <div className="w-1/2 h-2/4 bg-white mt-20">
+        <h2 className="bg-primary text-black text-content py-3 px-6 font-semibold">
+          Your data is safe
+        </h2>
+        <div className="text-black font-light px-6 py-3 banner-content">
+          <p>
+            Whatsapp allows users to &nbsp;
+            <a href="#how-to" className="underline text-accentDark">
+              export chat history
+            </a>
+            &nbsp; as a *.txt file. WhatsViz parses and analyses this data on
+            the client side (i.e
+            <span className="bg-accent font-semibold">
+              your chat data never leaves your browser
+            </span>
+            ), to create meaningful visualizations
+          </p>
+          <p className="mt-8">No data is saved on any server.</p>
+        </div>
+      </div>
+    </main>
+    <h1 id="how-to" className="text-heading-2 mt-12 font-extrabold leading-8">
+      How to export your chat history
+    </h1>
+    <div className="flex flex-wrap gap-11 mt-8">
+      <div className=" bg-white" style={{ maxWidth: '300px' }}>
+        <img src={guide1} alt="guide 1" />
+      </div>
+      <div className=" bg-white" style={{ maxWidth: '300px' }}>
+        <img src={guide2} alt="guide 2" />
+      </div>
+      <div className="  bg-white" style={{ maxWidth: '300px' }}>
+        <img src={guide3} alt="guide 3" />
+      </div>
+    </div>
+    <div className="text-md font-light leading-7 my-10 max-w-md">
+      Follow the steps and save the *.txt WhatsApp chat log file using your
+      preferred method. Then import the log to WhatsViz to see the
+      visualizations
+    </div>
+    <label
+      className="font-extrabold text-black text-content
+      bg-white px-8 py-2 rounded-md border-4 border-primary cursor-pointer
+      "
+    >
+      <span>IMPORT CHAT</span>
+      <input type="file" onChange={importChat} className="hidden" />
+    </label>
+    <footer
+      className="flex border-t-2 
+  font-light justify-between items-center
+  text-xs py-4 gap-14 mt-10"
+    >
+      <div className="flex-shrink-0">2021 © WhatsViz</div>
+      <div className="flex-shrink">
+        WhatsApp is a registered trademark of Facebook, Inc. This site and
+        service are not related in any way to Facebook Inc.
+      </div>
+      <div className="flex-shrink-0">Privacy Policy</div>
+    </footer>
+  </div>
+);
 export default ImportFileSteps;
--- src/MainPage.js
+++ src/MainPage.js
@@ -49,109 +49,107 @@ const MainPage = () => {
     reader.readAsText(file);
   };
 
-  return (
+  return formattedData.fileSelected === false ? (
+    <ImportFileSteps importChat={readFile} />
+  ) : (
     <>
-      {formattedData.fileSelected === false ? (
-        <ImportFileSteps />
-      ) : (
-        <div className="w-full h-full flex flex-col gap-6">
-          <ChatOverview
-            allMessages={formattedData.userMessages || []}
-            chatOverview={formattedData.chatOverview || {}}
-            isLoading={formattedData.isLoading}
-          />
-          <div className="flex flex-wrap gap-5">
-            <div className="flex flex-col card-bg h-96 w-full">
-              <div className="flex p-2 sm:p-4 text-md sm:text-xl">
-                <div className="flex-1 text-left font-heading">
-                  TOTAL MESSAGES PER USER
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5 inline text-purple-800"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M12 7a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0V8.414l-4.293 4.293a1 1 0 01-1.414 0L8 10.414l-4.293 4.293a1 1 0 01-1.414-1.414l5-5a1 1 0 011.414 0L11 10.586 14.586 7H12z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </div>
-                <div className="flex-1 text-right font-heading">
-                  {formattedData.isLoading ? (
-                    <div className="flex justify-end">
-                      <ThreeDots width="20" />
-                    </div>
-                  ) : (
-                    <>
-                      <span>{`${formattedData.msgPerUserCount[0].userName} - ${formattedData.msgPerUserCount[0].count} Messages`}</span>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-5 w-5 inline text-purple-800"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M12.395 2.553a1 1 0 00-1.45-.385c-.345.23-.614.558-.822.88-.214.33-.403.713-.57 1.116-.334.804-.614 1.768-.84 2.734a31.365 31.365 0 00-.613 3.58 2.64 2.64 0 01-.945-1.067c-.328-.68-.398-1.534-.398-2.654A1 1 0 005.05 6.05 6.981 6.981 0 003 11a7 7 0 1011.95-4.95c-.592-.591-.98-.985-1.348-1.467-.363-.476-.724-1.063-1.207-2.03zM12.12 15.12A3 3 0 017 13s.879.5 2.5.5c0-1 .5-4 1.25-4.5.5 1 .786 1.293 1.371 1.879A2.99 2.99 0 0113 13a2.99 2.99 0 01-.879 2.121z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                    </>
-                  )}
-                </div>
+      <div className="w-full h-full flex flex-col gap-6 p-5 font-content">
+        <ChatOverview
+          allMessages={formattedData.userMessages || []}
+          chatOverview={formattedData.chatOverview || {}}
+          isLoading={formattedData.isLoading}
+        />
+        <div className="flex flex-wrap gap-5">
+          <div className="flex flex-col card-bg h-96 w-full">
+            <div className="flex p-2 sm:p-4 text-md sm:text-xl">
+              <div className="flex-1 text-left font-heading">
+                TOTAL MESSAGES PER USER
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 inline text-purple-800"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12 7a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0V8.414l-4.293 4.293a1 1 0 01-1.414 0L8 10.414l-4.293 4.293a1 1 0 01-1.414-1.414l5-5a1 1 0 011.414 0L11 10.586 14.586 7H12z"
+                    clipRule="evenodd"
+                  />
+                </svg>
               </div>
-
-              <div className="flex-grow">
+              <div className="flex-1 text-right font-heading">
                 {formattedData.isLoading ? (
-                  <div className="flex justify-center items-center h-full">
-                    <Puff />
+                  <div className="flex justify-end">
+                    <ThreeDots width="20" />
                   </div>
                 ) : (
-                  <SimpleBar userMessageCount={formattedData.msgPerUserCount} />
+                  <>
+                    <span>{`${formattedData.msgPerUserCount[0].userName} - ${formattedData.msgPerUserCount[0].count} Messages`}</span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5 inline text-purple-800"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M12.395 2.553a1 1 0 00-1.45-.385c-.345.23-.614.558-.822.88-.214.33-.403.713-.57 1.116-.334.804-.614 1.768-.84 2.734a31.365 31.365 0 00-.613 3.58 2.64 2.64 0 01-.945-1.067c-.328-.68-.398-1.534-.398-2.654A1 1 0 005.05 6.05 6.981 6.981 0 003 11a7 7 0 1011.95-4.95c-.592-.591-.98-.985-1.348-1.467-.363-.476-.724-1.063-1.207-2.03zM12.12 15.12A3 3 0 017 13s.879.5 2.5.5c0-1 .5-4 1.25-4.5.5 1 .786 1.293 1.371 1.879A2.99 2.99 0 0113 13a2.99 2.99 0 01-.879 2.121z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </>
                 )}
               </div>
             </div>
-            <div className="card-bg  h-96 w-full">
+
+            <div className="flex-grow">
               {formattedData.isLoading ? (
                 <div className="flex justify-center items-center h-full">
                   <Puff />
                 </div>
               ) : (
-                <WordCloud
-                  key={formattedData.userMessages}
-                  wordFreqPerUser={formattedData.wordFreqPerUser}
-                  userNames={formattedData.userNames}
-                  isLoading={formattedData.isLoading}
-                />
+                <SimpleBar userMessageCount={formattedData.msgPerUserCount} />
               )}
             </div>
-            {/* <div className="card-bg  h-96 w-full">
-              {userMessages && userNames && (
-                <DailyMessages
-                  allMessages={userMessages}
-                  userNames={userNames}
-                />
-              )}
-            </div> */}
-            {/* <div className="card-bg  h-96 w-full">
-              {userMessages && userNames && (
-                <RaceBarChartContainer
-                  userDayWiseMessageCount={getRacingBarData(userMessages)}
-                  allUsers={userNames}
-                />
-              )}
-            </div> */}
           </div>
+          <div className="card-bg  h-96 w-full">
+            {formattedData.isLoading ? (
+              <div className="flex justify-center items-center h-full">
+                <Puff />
+              </div>
+            ) : (
+              <WordCloud
+                key={formattedData.userMessages}
+                wordFreqPerUser={formattedData.wordFreqPerUser}
+                userNames={formattedData.userNames}
+                isLoading={formattedData.isLoading}
+              />
+            )}
+          </div>
+          {/* <div className="card-bg  h-96 w-full">
+          {userMessages && userNames && (
+            <DailyMessages
+              allMessages={userMessages}
+              userNames={userNames}
+            />
+          )}
+        </div> */}
+          {/* <div className="card-bg  h-96 w-full">
+          {userMessages && userNames && (
+            <RaceBarChartContainer
+              userDayWiseMessageCount={getRacingBarData(userMessages)}
+              allUsers={userNames}
+            />
+          )}
+        </div> */}
         </div>
-      )}
+      </div>
 
-      <footer className="fixed bottom-5 right-5">
+      <footer className="fixed bottom-5 right-5 font-content">
         {/* hack for styling browse files button */}
         <label
           className="rounded-full bg-white h-10 sm:h-12 cursor-pointer w-32 text-purple-800 hover:bg-gray-200
-                    flex justify-center items-center"
+                  flex justify-center items-center"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
